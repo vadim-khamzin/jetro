76e7e49fc798a65a83b63759105066a71fc109d2
Vadim Khamzin <vkhamzin94@gmail.com>
2018-09-19 07:43:36 +0300

    Initial gh-pages commit

--- app/blocks/menu/menu.js
+++
@@ -1,5 +0,0 @@
-import $ from 'jquery';
-
-$('.dropdown').on('click', function() {
-	$('.menu').toggleClass('menu_dropped', 3000, "easeIn");
-});
--- app/scripts/app.js
+++
@@ -1,7 +0,0 @@
-import svg4everybody from 'svg4everybody';
-import '../blocks/slider/slider';
-import '../blocks/menu/menu';
-
-$(() => {
-	svg4everybody();
-});
